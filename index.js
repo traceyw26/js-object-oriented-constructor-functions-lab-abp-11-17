1081f77c614e52fdd6f92e5581d3f699e3f4dbcd
Tracey <tracey.wilhelmsen@gmail.com>
2017-11-12 19:48:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function Scooter(year, color, model) {
   this.color = color
   this.model = model
 }
+
+function Driver(name, age, experience) {
+  this.name = name
+  this.age = age
+  this.experience = experience
+  
+}
