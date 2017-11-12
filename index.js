8af61ddda36012f2dcb2034af243cb5e4fa3ae6f
Tracey <tracey.wilhelmsen@gmail.com>
2017-11-12 19:49:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,10 @@ function Driver(name, age, experience) {
   this.name = name
   this.age = age
   this.experience = experience
-  
+
+}
+
+function PickupLocation(address, city) {
+  this.address = address
+  this.city = city
 }
